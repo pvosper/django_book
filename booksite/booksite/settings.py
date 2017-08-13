4b8e8f89739a9b7dc4506a8c8e9e8bc26b5606f3
pvosper <pvosper@mac.com>
2017-08-13 07:35:39 -0700

    Ch 4 my solution (template example doesn't support 1.8)

--- booksite/booksite/settings.py
+++ booksite/booksite/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'booksite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'booksite/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- booksite/booksite/views.py
+++ booksite/booksite/views.py
@@ -1,3 +1,6 @@
+from django.shortcuts import render
+from django.template.loader import get_template
+from django.template import Context
 from django.http import Http404, HttpResponse
 import datetime
 # from django.utils import timezone
@@ -10,8 +13,12 @@ def hello(request):
 def current_datetime(request):
     now = datetime.datetime.now()
     # now = timezone.now()  # Doesn't use settings timezone
-    html = "<html><body>It is now %s.</body></html>" % now
-    return HttpResponse(html)
+    # t = get_template('current_datetime.html')
+    # html = t.render(Context({'current_date': now}))  # Not Django 1.8+
+    # html = "<html><body>It is now %s.</body></html>" % now
+    # return HttpResponse(html)
+    context = {'current_date': now}
+    return render(request, 'current_datetime.html', context)
 
 def hours_ahead(request, offset):
     try:
