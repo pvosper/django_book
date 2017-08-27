0c6b8686fe49e1ef54cda45f6037b1d485dd1ff8
pvosper <pvosper@mac.com>
2017-08-27 08:06:28 -0700

    Chapter 5 complete; WTF __unicode__ vs __str__ anyway

--- booksite/books/models.py
+++ booksite/books/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Publisher(models.Model):
     name = models.CharField(max_length=30)
     address = models.CharField(max_length=50)
@@ -8,11 +9,26 @@ class Publisher(models.Model):
     country = models.CharField(max_length=50)
     website = models.URLField()
 
+    def __unicode__(self):
+        return self.name
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+
 
 class Author(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=40)
     email = models.EmailField()
+
+    def __unicode__(self):
+        return u'%s %s' % (self.first_name, self.last_name)
+
+    def __str__(self):
+        return u'%s %s' % (self.first_name, self.last_name)
 
 
 class Book(models.Model):
@@ -21,3 +37,8 @@ class Book(models.Model):
     publisher = models.ForeignKey(Publisher)
     publication_date = models.DateField()
 
+    def __unicode__(self):
+        return self.title
+
+    def __str__(self):
+        return self.title
