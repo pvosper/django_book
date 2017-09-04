e19c5c5454a733279e1693229247716a1043eb84
pvosper <pvosper@mac.com>
2017-09-04 06:38:57 -0700

    Chapter 7 work

--- booksite/books/views.py
+++ booksite/books/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
-from django.shortcuts import render, render_to_response
+from django.shortcuts import render_to_response
+from books.models import Book
 
 
 def search_form(request):
@@ -7,8 +8,10 @@ def search_form(request):
 
 
 def search(request):
-    if 'q' in request.GET:
-        message = 'You searched for: %r' % request.GET['q']
+    if 'q' in request.GET and request.GET['q']:
+        q = request.GET['q']
+        books = Book.objects.filter(title__icontains=q)
+        return render_to_response('search_results.html',
+            {'books': books, 'query': q})
     else:
-        message = 'You submitted and empty form'
-    return HttpResponse(message)
+        return HttpResponse('Please submit a search term')
